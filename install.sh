60c75f46c7313c696d51042cffa27434725277a5
Alfanhui <program.foo@gmail.com>
2018-08-29 23:11:24 +0100

    Install.sh now updated

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update && apt-get upgrade -y
+sudo apt-get dist-upgrade
 sudo apt-get install python-dev
 sudo apt-get install python-pip
 sudo pip install evdev
-sudo pip install bluetool 
-sudo pip install playsound
 sudo apt-get install python-gi gir1.2-gst-plugins-base-1.0 gir1.2-gtk-2.0 gir1.2-gtk-3.0
-sudo cp /home/pi/Desktop/source.py /etc/init.d/
+sudo apt-get install pulseaudio pulseaudio-module-bluetooth vlc blueman
 echo "Install complete, please restart"
